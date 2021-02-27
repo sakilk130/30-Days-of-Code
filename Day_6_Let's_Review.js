5df333399f11bd5ca01c13c50438bc861438ce8c
Sakil Khan <sakilk130@gmail.com>
2021-02-27 11:14:48 +0600

    complete day_6

---
+++ Day_6_Let's_Review.js
@@ -0,0 +1,23 @@
+function processData(input) {
+  //Enter your code here
+  let inputCopy = input.split('\n').slice(1);
+  inputCopy.forEach((str) => {
+    let evenStr = '';
+    let oddStr = '';
+    for (let i = 0; i < str.length; i++) {
+      i % 2 ? (oddStr += str[i]) : (evenStr += str[i]);
+    }
+    console.log(`${evenStr} ${oddStr}`);
+  });
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+_input = '';
+process.stdin.on('data', function (input) {
+  _input += input;
+});
+
+process.stdin.on('end', function () {
+  processData(_input);
+});
